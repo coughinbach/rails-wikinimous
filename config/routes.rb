ded8afec445d1f0680ef9781e7bc195b51a0f551
Lola Barbier <lolabarbier@hotmail.com>
2017-11-22 13:44:21 +0100

    made a little article list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  resources :articles
+
+  root to: 'articles#index'
+
+  # get 'articles/index'
+
+  # get 'articles/show'
+
+  # get 'articles/new'
+
+  # get 'articles/edit'
+
+  # get 'articles/create'
+
+  # get 'articles/update'
+
+  # get 'articles/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts 'Creating 10 totally genuine articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Seinfeld.character,
+    content: Faker::Seinfeld.quote,
+  )
+  article.save
+end
+puts 'Done-zo!'
